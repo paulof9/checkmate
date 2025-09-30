87bdd80e98311330e3994c03fa95d3a19d814c68
paulof9 <above2@Outlook.com>
2025-09-30 17:23:52 -0300

    feat:POST route to add notes

--- main.js
+++ main.js
@@ -2,10 +2,24 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
+// middleware para express interpretar JSON
+app.use(express.json());
+
 let notes = [
   { id: 1, content: 'First note', complete: '1' },
   { id: 2, content: 'Second note', complete: '0' }
 ];
+
+app.post('/notes', (req, res) => {
+    const newNote = {
+        id: notes.length + 1,
+        ...req.body,
+        complete: '0'
+    };
+    notes.push(newNote);
+    res.json({ content: newNote });
+    console.log('New note added:', newNote);
+});
 
 app.get('/notes/info', (req, res) => {
     const totalNotes = notes.length;
