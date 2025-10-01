c56ec108a8a9218dd2b711cb562737e5a63408bf
paulof9 <above2@Outlook.com>
2025-09-30 21:08:03 -0300

    docs: some explanations

--- main.js
+++ main.js
@@ -2,21 +2,24 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
-// middleware para express interpretar JSON
+// middleware to interpret express JSON
 app.use(express.json());
 
+// In-memory data store (not for production use)
 let notes = [
   { id: 1, content: 'First note', complete: '1' },
   { id: 2, content: 'Second note', complete: '0' }
 ];
 
+//DELETE
 app.delete('/notes/:id', (req, res) => {
     const id = Number(req.params.id);
-    notes = notes.filter(note => note.id !== id);
+    notes = notes.filter(note => note.id !== id); // filter to remove the note, all other notes remain except the requested one
     console.log(`Note with id ${id} deleted.`);
 
 });
 
+//UPDATE
 app.put("/notes/:id", (req, res) => {
     const id = Number(req.params.id);
     const noteIndex = notes.findIndex(note => note.id === id);
@@ -24,6 +27,7 @@ app.put("/notes/:id", (req, res) => {
     console.log('Note updated:', notes[noteIndex]);
 });
 
+//CREATE
 app.post('/notes', (req, res) => {
     const newNote = {
         id: notes.length + 1,
@@ -35,16 +39,19 @@ app.post('/notes', (req, res) => {
     console.log('New note added:', newNote);
 });
 
+//READ INFO
 app.get('/notes/info', (req, res) => {
     const totalNotes = notes.length;
     const completeNotes = notes.filter(note => note.complete === '1'); // note is the index 'i'
     res.json({ total: totalNotes, complete: completeNotes.length });
 });
 
+//READ ALL
 app.get('/notes/list', (req, res) => {
     res.json(notes);
 });
 
+//READ ONE
 app.get('/notes/:id', (req, res) => {
     const id = Number(req.params.id);
     const note = notes.filter(note => note.id === id);
@@ -55,6 +62,7 @@ app.get('/notes/:id', (req, res) => {
     }
 });
 
+// MAIN PAGE ROUTE
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
